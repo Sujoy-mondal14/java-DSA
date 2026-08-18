2dee47d36d7cd5aba4866ccc32147f245c4016b0
Sujoy Mondal <sujoymondal1418852@gmail.com>
2026-08-18 17:21:29 +0530

    edge case of ceiling added

--- src/src/com/Binary_Search/CeilingOfNumber.java
+++ src/src/com/Binary_Search/CeilingOfNumber.java
@@ -4,14 +4,16 @@ public class CeilingOfNumber {
     //ceiling mean the smallest element array but not greater or equal to the target element
     static void main() {
         int[] arr = {2,4,8,12,14,18,20};
-        int target = 3;
+        int target = 1;
         System.out.println(celingofNumber(arr, target));
     }
 
     static int celingofNumber(int[] arr, int target){
+        //if the target element is greater than max element of array
+        if(target > arr[arr.length-1]) return arr[arr.length-1];
+
         int start = 0;
         int end = arr.length - 1;
-
 
         while(start <= end){
             int mid = start + ( end-start)/2 ;
