283876fff057733b4ffc3ebaf2adfed84cd78347
Sujoy Mondal <sujoymondal1418852@gmail.com>
2026-08-18 17:22:08 +0530

    floor number added

---
+++ src/src/com/Binary_Search/FloorOfNumber.java
@@ -0,0 +1,27 @@
+package com.Binary_Search;
+
+public class FloorOfNumber {
+    static void main() {
+        int[] arr = {2,4,8,12,14,18,20};
+        int target = 99;
+        System.out.println(floorOfNumber(arr, target));
+    }
+    static int floorOfNumber(int[] arr, int target) {
+        //if taget element is smaller than the smallest element of the array
+        if(target < arr[0]) return arr[0];
+
+        int start = 0;
+        int end = arr.length - 1;
+
+        while(start <= end){
+            int mid = start + (end -start)/2;
+
+            if(arr[mid] == target) return target;
+
+            if(target > arr[mid]) start = mid + 1;
+            else if(target < arr[mid]) end = mid -1;
+        }
+
+        return arr[end];
+    }
+}
